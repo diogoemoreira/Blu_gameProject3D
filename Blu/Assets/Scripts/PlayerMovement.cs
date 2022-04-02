4f1f648bab22f80e8e1f7c99702e06e21ddfb508
diogoemoreira <diogo.e.moreira@hotmail.com>
2022-04-02 16:46:29 +0100

    Added movement and jumping

--- Blu/Assets/Scripts/PlayerMovement.cs
+++ Blu/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController charControl;
+    
+    public float speed = 3f;
+    public float gravity = -20f;
+    public float jumpHeight = 3f;
+    
+    public Transform groundCheck; //center of the ground check sphere
+    public float groundDistance = 0.4f; //radius of the sphere which will check if we are on the ground
+    public LayerMask groundMask; //to control what objects the sphere should check for
+    public bool onGround=true;
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
-        
+        onGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(onGround && velocity.y <0){
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        charControl.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && onGround){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        charControl.Move(velocity * Time.deltaTime);
     }
 }
