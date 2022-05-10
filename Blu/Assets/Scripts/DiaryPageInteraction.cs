bebeb5d2c108f2a83ffc9c0232c62b05a46314e2
Miguel Silva <miguel.s.silva@ua.pt>
2022-05-10 18:03:58 +0100

    Basic diary interaction flow

--- Blu/Assets/Scripts/DiaryPageInteraction.cs
+++ Blu/Assets/Scripts/DiaryPageInteraction.cs
@@ -6,14 +6,32 @@ public class DiaryPageInteraction : InteractableUseItem
 {
     private Camera playerCamera;
     public GameObject pagePrefab;
+    private GameObject pagina;
     private void Start()
     {
         playerCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();   
     }
+
+    private void Update()
+    {
+        if (pagina != null)
+        {
+            if (Input.GetButtonDown("Cancel"))
+            {
+                Destroy(pagina);
+                pagina = null;
+                playerCamera.GetComponent<MouseLook>().enabled = true;
+                InteractionManager.instance.DisplayInteractionText(this.gameObject);
+            }
+        }
+    }
+
     protected override void Execute()
     {
+        if (pagina != null) { return; }
         InteractionManager.instance.StopDisplayInteractText(this.gameObject);
-        GameObject page = Instantiate(pagePrefab, playerCamera.transform.position + playerCamera.transform.forward * 0.5f, playerCamera.transform.rotation);
-        page.transform.Rotate(-90,0,0);
+        pagina = Instantiate(pagePrefab, playerCamera.transform.position + playerCamera.transform.forward * 0.5f, playerCamera.transform.rotation);
+        pagina.transform.Rotate(-90,0,0);
+        playerCamera.GetComponent<MouseLook>().enabled = false;
     }
 }
