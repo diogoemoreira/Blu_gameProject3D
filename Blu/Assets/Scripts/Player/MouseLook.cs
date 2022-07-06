a3dda1358cc70e85d14fd7f0e0c12dec39ed0a3f
diogoemoreira <diogo.e.moreira@hotmail.com>
2022-07-06 16:02:55 +0100

    New pantry added

--- Blu/Assets/Scripts/Player/MouseLook.cs
+++ Blu/Assets/Scripts/Player/MouseLook.cs
@@ -20,7 +20,7 @@ public class MouseLook : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0 , 0);
+        transform.rotation = Quaternion.Euler(xRotation, 0 , 0);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
