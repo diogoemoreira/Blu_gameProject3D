8b1312a96eaaeeef835078d44ecc6997243e5937
kpdzhnv <nic.a@mail.ru>
2022-07-07 13:37:53 +0300

    Added Post processing effects
    - post processing for high heart rate
    - post processing for planters light

--- Blu/Assets/Scripts/UI/HeartBeatUI.cs
+++ Blu/Assets/Scripts/UI/HeartBeatUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
 public class HeartBeatUI : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class HeartBeatUI : MonoBehaviour
     private Vector3 maxSize = new Vector3(1f, 1f, 1f);
     private Vector3 minSize = new Vector3(0.8f, 0.8f, 0.8f);
     private float rate;
+    private float post_processing_value;
+    public PostProcessVolume postProcessVolume;
+    private Vignette vignette;
+    private ChromaticAberration chromaticAberration;
 
     private void Awake()
     {
@@ -31,6 +36,16 @@ public class HeartBeatUI : MonoBehaviour
         rate = 20f;
 
         this.gameObject.SetActive(false);
+
+        // Post processing enable:
+        postProcessVolume.profile.TryGetSettings(out vignette);
+        postProcessVolume.profile.TryGetSettings(out chromaticAberration);
+
+        vignette.active = true;
+        vignette.intensity.value = 0;
+
+        chromaticAberration.active = true;
+        chromaticAberration.intensity.value = 0;
     }
 
     private void Update()
@@ -40,7 +55,8 @@ public class HeartBeatUI : MonoBehaviour
             if (increasingSize)
             {
                 heartBeatImage.transform.localScale = Vector3.Lerp(heartBeatImage.transform.localScale, maxSize, Time.deltaTime * rate);
-
+                // too fast for vignette ->
+                //vignette.intensity.value = Mathf.Lerp(rate / 100.0f, rate / 100.0f + 0.2f, Time.deltaTime * rate);
                 if (heartBeatImage.transform.localScale == maxSize)
                 {
                     increasingSize = false;
@@ -49,13 +65,28 @@ public class HeartBeatUI : MonoBehaviour
             else
             {
                 heartBeatImage.transform.localScale = Vector3.Lerp(heartBeatImage.transform.localScale, minSize, Time.deltaTime * (rate*0.7f));
-
+                // too fast for vignette ->
+                //vignette.intensity.value = Mathf.Lerp(rate / 100.0f + 0.2f, rate / 100.0f, Time.deltaTime * rate);
                 if (heartBeatImage.transform.localScale == minSize)
                 {
                     increasingSize = true;
                 }
-            }    
+            }
         }
+
+        // Change vignette intensity using a sinus curve & add Chromatic Aberration 
+        int slow_speed = 5, fast_speed = 10; // check these values please, maybe my framerate affects the heart speed
+
+        if (rate > 170)
+            vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup * fast_speed) / 2.0f + 0.5f;
+        else if (rate > 120)
+        {
+            vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup * slow_speed) / 2.0f + 0.5f;
+            chromaticAberration.intensity.value = 0.5f;
+        }
+        else
+            vignette.intensity.value = 0;
+
     }
 
     public void UpdateHeartBeat(int heartBeat)
