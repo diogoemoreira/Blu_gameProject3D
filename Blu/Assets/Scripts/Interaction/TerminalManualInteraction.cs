ba2b9ade880ed0faaf1f5c4209301a7a7641476b
diogoemoreira <diogo.e.moreira@hotmail.com>
2022-07-08 01:27:50 +0100

    Manual showing correct cable order

--- Blu/Assets/Scripts/Interaction/TerminalManualInteraction.cs
+++ Blu/Assets/Scripts/Interaction/TerminalManualInteraction.cs
@@ -13,7 +13,7 @@ public class TerminalManualInteraction : InteractableUseItem
     private void Start() {
         int[] c =TerminalPuzzle.instance.GetCableOrder();
         string cables = colors[c[0]]+", "+colors[c[1]]+", "+colors[c[2]];
-        manual_text.text = "Analogic system:\n- Turn off power\n- Open door terminal\n(remove screws)\n- Remove Wires "+cables+"\n- Insert 6 digit code\n(default code is 000000)";
+        manual_text.SetText("Analogic system:\n- Turn off power\n- Open door terminal\n(remove screws)\n- Remove Wires:\n"+cables+"\n- Insert 6 digit code\n(default code is 000000)");
     }
 
     protected override void Execute()
